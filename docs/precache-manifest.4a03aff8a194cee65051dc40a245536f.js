aaeca1fd187efa0ee5a5dd8068348d2bc72fbd3b
mgracesb <mercysbarcel@gmail.com>
2020-04-25 14:30:53 +0200

    create production files

---
+++ docs/precache-manifest.4a03aff8a194cee65051dc40a245536f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b673dc77af5282014b9c82cc0782e965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d216c346c09f482a7483",
+    "url": "/static/css/main.c4833033.chunk.css"
+  },
+  {
+    "revision": "e3afcdcf9916e07f42a1",
+    "url": "/static/js/2.25c193ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25c193ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d216c346c09f482a7483",
+    "url": "/static/js/main.2ed79878.chunk.js"
+  },
+  {
+    "revision": "db6e35ea8234e5f73134",
+    "url": "/static/js/runtime-main.1702a5b0.js"
+  },
+  {
+    "revision": "45d3ba15c51989263d3ad853f2279bb2",
+    "url": "/static/media/alien.45d3ba15.svg"
+  },
+  {
+    "revision": "ea770f1679366be71ce418b9aa18bb0a",
+    "url": "/static/media/alive.ea770f16.png"
+  },
+  {
+    "revision": "60a2e5769604db00606a4560bc9927dc",
+    "url": "/static/media/bg.60a2e576.jpg"
+  },
+  {
+    "revision": "72b0f1950c0bddf245ba78f5fed9c0df",
+    "url": "/static/media/bg2.72b0f195.jpg"
+  },
+  {
+    "revision": "def585c94780011853759bd09701d302",
+    "url": "/static/media/dead.def585c9.svg"
+  },
+  {
+    "revision": "9bfcd5fca9afd6080b11fdbabcf891fc",
+    "url": "/static/media/human.9bfcd5fc.svg"
+  },
+  {
+    "revision": "59605127dffb786ac6bcb369a44f4e09",
+    "url": "/static/media/rick-morty.59605127.png"
+  },
+  {
+    "revision": "5893f9459c9c5baaf4e16b40154ab393",
+    "url": "/static/media/warning.5893f945.gif"
+  }
+]);
