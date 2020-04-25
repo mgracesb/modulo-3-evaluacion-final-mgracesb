e371adfafc9239f00f239ad6dd9b63f98575c4e3
mgracesb <mercysbarcel@gmail.com>
2020-04-25 16:41:23 +0200

    add styles to wrongURL message

--- src/components/app-components/Wrong.js
+++ src/components/app-components/Wrong.js
@@ -1,7 +1,14 @@
 import React from "react";
+import warning from "../../images/warning.svg";
+import "../../stylesheets/WrongURL.scss";
 
 const Wrong = () => {
-  return <div>NO HAY Ná</div>;
+  return (
+    <div className="wrongURL">
+      <img src={warning} alt="not found" className="warningImg" />
+      <p className="warningText">"El personaje que buscas no existe"</p>
+    </div>
+  );
 };
 
 export default Wrong;
