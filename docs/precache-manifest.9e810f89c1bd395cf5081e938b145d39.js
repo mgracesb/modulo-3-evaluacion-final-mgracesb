013094bcb5114d4abeaa52dbb4e8cbb056d2c0d0
mgracesb <mercysbarcel@gmail.com>
2020-04-25 23:54:57 +0200

    create production files

---
+++ docs/precache-manifest.9e810f89c1bd395cf5081e938b145d39.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74329da8d6725e86c4801cfdf921b0b8",
+    "url": "/modulo-3-evaluacion-final-mgracesb/index.html"
+  },
+  {
+    "revision": "8a0c859e27fe5878f082",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/css/main.e738a9c9.chunk.css"
+  },
+  {
+    "revision": "c333af299d4705af4b8b",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/js/2.209709d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/js/2.209709d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0c859e27fe5878f082",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/js/main.5a3a350e.chunk.js"
+  },
+  {
+    "revision": "beab67df2eaaa9842f37",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/js/runtime-main.92f2a27d.js"
+  },
+  {
+    "revision": "45d3ba15c51989263d3ad853f2279bb2",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/alien.45d3ba15.svg"
+  },
+  {
+    "revision": "ea770f1679366be71ce418b9aa18bb0a",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/alive.ea770f16.png"
+  },
+  {
+    "revision": "60a2e5769604db00606a4560bc9927dc",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/bg.60a2e576.jpg"
+  },
+  {
+    "revision": "72b0f1950c0bddf245ba78f5fed9c0df",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/bg2.72b0f195.jpg"
+  },
+  {
+    "revision": "def585c94780011853759bd09701d302",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/dead.def585c9.svg"
+  },
+  {
+    "revision": "9bfcd5fca9afd6080b11fdbabcf891fc",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/human.9bfcd5fc.svg"
+  },
+  {
+    "revision": "25fcdcfbb4b91e4e63e5f6925ef6ef7c",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/reset.25fcdcfb.png"
+  },
+  {
+    "revision": "59605127dffb786ac6bcb369a44f4e09",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/rick-morty.59605127.png"
+  },
+  {
+    "revision": "5893f9459c9c5baaf4e16b40154ab393",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/warning.5893f945.gif"
+  },
+  {
+    "revision": "b1b1b58cc210bbbaa6052da5923707e2",
+    "url": "/modulo-3-evaluacion-final-mgracesb/static/media/warning.b1b1b58c.svg"
+  }
+]);
